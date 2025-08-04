96f4cf340fee28f0d5f57bafb8557c067745c32e
NandoGonz <168700146+NandoGonz@users.noreply.github.com>
2025-08-03 20:52:46 -0500

    Corrección del error trailing_whitespace

--- soluciones/ejercicio_28.py
+++ soluciones/ejercicio_28.py
@@ -1,4 +1,4 @@
-'''CLASE SingletonMeta HEREDA DE type
+"""CLASE SingletonMeta HEREDA DE type
   _instancias = {}
   METODO __call__(...)
     SI clase NO ESTA EN _instancias
@@ -6,23 +6,30 @@
     FIN SI
     RETORNAR _instancias[clase]
   FIN METODO
-FIN CLASE'''
+FIN CLASE"""
+
 
 
 class SingletonMeta(type):
+    # Diccionario para almacenar instancias únicas por clase
     _instancia = {}
 
     def __call__(cls, *args, **kwargs):
+        # Si la clase no tiene instancia, crea una nueva
         if cls not in cls._instancia:
             cls._instancia[cls] = super().__call__(*args, **kwargs)
+        # Retorna la instancia única
         return cls._instancia[cls]
 
 
 class Misingleton(metaclass=SingletonMeta):
+    # Clase que utiliza el patrón Singleton
     pass
 
 
 if __name__ == "__main__":
+    # Se crean dos instancias de Misingleton
     a = Misingleton()
     b = Misingleton()
+    # Se verifica que ambas variables apunten a la misma instancia
     print(a is b)
